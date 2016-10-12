538bba2bec8d2418f25ccc0a0020bed06395fb2b
sandeep <sandeep@rigup.co>
2016-10-12 11:23:10 -0500

    Pass entire saml response to validator
    Conflicts:
    	lib/devise_saml_authenticatable.rb

--- lib/devise_saml_authenticatable/model.rb
+++ lib/devise_saml_authenticatable/model.rb
@@ -40,7 +40,7 @@ module Devise
           auth_value.try(:downcase!) if Devise.case_insensitive_keys.include?(key)
 
           if Devise.saml_auth_value_validator
-            if not Devise.saml_auth_value_validator.new.validate(saml_response.settings.idp_entity_id, auth_value)
+            if not Devise.saml_auth_value_validator.new.validate(saml_response, auth_value)
               logger.info("User(#{auth_value}) did not pass custom validation.")
               return nil
             end
